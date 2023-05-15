74defa772f43300b3d0c9b746867e4e3f138b28e
Taichi Nishimura <awkrail01@gmail.com>
2023-05-15 20:03:55 +0900

    impled flip function

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,9 @@ use tgaimage::{TGAColor, TGAImage, Format};
 fn main() {
     let white: TGAColor = TGAColor::new(255, 255, 255, 255, Format::RGBA);
     let red: TGAColor = TGAColor::new(255, 0, 0, 255, Format::RGBA);
-    let image = TGAImage::new(100, 100, Format::RGB);
+
+    let mut image = TGAImage::new(100, 100, Format::RGB);
+    image.set(52, 51, red);
+    image.flip_vertically();
+    //image.write_tga_file("output.tga");
 }
--- src/tgaimage.rs
+++ src/tgaimage.rs
@@ -45,8 +45,105 @@ impl TGAImage {
         }
     }
 
+    pub fn set(&mut self, x: i32, y: i32, c: TGAColor) -> bool {
+        if self.data.len() == 0 || x < 0 || y < 0 || x >= self.width || y >= self.height {
+            return false;
+        }
+        for i in 0..self.bytespp {
+            let pos = (i + (x + y * self.width) * self.bytespp) as usize;
+            self.data[pos] = c.b;
+            self.data[pos+1] = c.g;
+            self.data[pos+2] = c.r;
+            self.data[pos+3] = c.a;
+        }
+        return true;
+    }
+
+    pub fn flip_vertically(&mut self) -> bool {
+        if self.data.len() == 0 {
+            return false;
+        }
+
+        let bytes_per_line: usize = (self.width * self.bytespp).try_into().unwrap_or(0);
+        if bytes_per_line == 0 {
+            return false;
+        }
+
+        let mut line: Vec<u8> = vec![0; bytes_per_line];
+        let half: usize = (self.height >> 1).try_into().unwrap_or(0);
+        if half == 0 {
+            return false; // image size is reasonably big
+        }
+
+        for i in 0..half {
+            let height = self.height as usize;
+            let l1 = i * bytes_per_line;
+            let l2 = (height-1-i) * bytes_per_line;
+            
+            // line <- data+l1
+            for j in 0..bytes_per_line {
+                line[j] = self.data[j + l1];
+                self.data[j + l1] = self.data[j + l2];
+                self.data[j + l2] = line[j];
+            }
+        }
+        return true;
+    }
+
+    /*
+    pub fn write_tga_file(&self, filename: &str) -> bool {
+        let developer_area_ref: [u8; 4] = [0, 0, 0, 0];
+        let extension_area_ref: [u8; 4] = [0, 0, 0, 0];
+        let footer: [u8; 17] = ['T','R','U','E','V','I','S','I','O','N','-','X','F','I','L','E','.'];
+        let datatypecode = get_datatype_code(&self.bytespp);
+        let header: TGAHeader = TGAHeader::new(0, 0, datatypecode, 0, 0, 0, 0, 0, 
+            self.width, self.height, self.bytespp << 3, 0x20);
+
+        // TODO: dump data
+    }
+    */
 }
 
+/*
+struct TGAHeader {
+    idlength: u8,
+    colormaptype: u8,
+    datatypecode: u8,
+    colormaporigin: u16,
+    colormaplength: u16,
+    colormapdepth: u8,
+    x_origin: u16,
+    y_origin: u16,
+    width: u16,
+    height: u16,
+    bitsperpixel: u8,
+    imagedescriptor: u8,
+}
+
+impl TGAHeader {
+    pub fn new(idlength: u8, colormaptype: u8, datatypecode: u8, colormaporigin: u16, colormaplength: u16,
+        colormapdepth: u8, x_origin: u16, y_origin: u16, width: u16, height: u16, bitsperpixel: u8, imagedescriptor: u8) {
+        TGAHeader {
+            idlength: idlength,
+            colormaptype: colormaptype,
+            datatypecode: datatypecode,
+            colormaporigin: colormaporigin,
+            colormaplength: colormaplength,
+            colormapdepth: colormapdepth,
+            x_origin: x_origin,
+            y_origin: y_origin,
+            width: width,
+            height: height,
+            bitsperpixel: bitsperpixel,
+            imagedescriptor: imagedescriptor,
+        }
+    }
+}
+*/
+
+/*
+ * util
+ */
 fn enum2num(bpp: Format) -> i32 {
     match bpp {
         Format::Grayscale => 1,
